1b9b86f70898bb011b6a4ca12ce81858796c04ae
LanaJeanDesigns <aaul0005@student.monash.edu>
2025-06-06 09:15:22 +1000

    update

---
+++ class-changes.js
@@ -0,0 +1,15 @@
+const classChanges = [
+  { time: 31, target: ".pop1", add: "active" },
+  { time: 33, target: ".pop1", remove: "active" },
+  
+  { time: 59, target: ".page", add: "active" },
+  { time: 59, target: ".bluebg", add: "bgwhite" },
+
+
+  { time: 16, target: ".changeme2", remove: "show" },
+  { time: 59, target: ".whitetext", add: "bluetext" },
+
+  { time: 45, target: ".pop2", add: "active" },
+  { time: 48, target: ".pop2", remove: "active" },
+  { time: 28, target: ".changeme3", add: "hidden" }
+];
