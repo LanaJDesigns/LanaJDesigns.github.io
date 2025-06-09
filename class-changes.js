5785f3c1974b9179991dcd8fa947bf8fe819f9d9
LanaJeanDesigns <aaul0005@student.monash.edu>
2025-06-09 18:14:14 +1000

    update

--- class-changes.js
+++ class-changes.js
@@ -1,15 +1,37 @@
 const classChanges = [
-  { time: 31, target: ".pop1", add: "active" },
+  { time: 30, target: ".pop1", add: "active" },
   { time: 33, target: ".pop1", remove: "active" },
   
   { time: 59, target: ".page", add: "active" },
   { time: 59, target: ".bluebg", add: "bgwhite" },
-
-
-  { time: 16, target: ".changeme2", remove: "show" },
   { time: 59, target: ".whitetext", add: "bluetext" },
 
-  { time: 45, target: ".pop2", add: "active" },
-  { time: 48, target: ".pop2", remove: "active" },
-  { time: 28, target: ".changeme3", add: "hidden" }
+  { time: 44, target: ".pop2", add: "active" },
+  { time: 50, target: ".pop2", remove: "active" },
+
+  { time: 94, target: ".treebox", add: "active" },
+  { time: 94, target: ".notes", add: "active" },
+  { time: 97, target: ".notes", remove: "active" },
+  { time: 97, target: ".art", add: "active" },
+  { time: 98, target: ".art", remove: "active" },
+  { time: 98, target: ".exist", add: "active" },
+  { time: 100, target: ".exist", remove: "active" },
+  { time: 100, target: ".treebox", remove: "active" },
+
+  { time: 151, target: ".well", add: "active" },
+  { time: 156, target: ".well", remove: "active" },
+
+  { time: 179, target: ".profilelink", add: "active" },
+  { time: 191, target: ".profilelink", remove: "active" },
+
+  { time: 309, target: ".afghanistan", add: "active" },
+  { time: 311, target: ".brunch", add: "active" },
+  { time: 314, target: ".afghanistan", remove: "active" },
+  { time: 314, target: ".brunch", remove: "active" },
+
+  { time: 354, target: ".blackbackdrop", remove: "popup" },
+  { time: 354, target: ".finaltextpopup", remove: "popup" },
+  { time: 358, target: ".blackbackdrop", add: "popup" },
+  { time: 358, target: ".finaltextpopup", add: "popup" },
+
 ];
